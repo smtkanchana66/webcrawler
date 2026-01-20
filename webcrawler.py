d7d54a10fd4de14434ad34fb2f5468e50a3339b6
smtkanchana66 <ict20066@std.uwu.ac.lk>
2026-01-20 08:13:28 +0530

    Add all finded link into new list

--- webcrawler.py
+++ webcrawler.py
@@ -6,10 +6,10 @@ fhand = urllib.request.urlopen('https://pahe.ink/')
 
 for line in fhand:
     decoded_line = line.decode('utf-8', errors='ignore').strip()
-    url = re.findall('https?://\\S+', decoded_line)
-    print(url)
-#     if url:
-#         newurl = url[0]
-#         url_lst.extend(url)
+    url = re.findall('https?://\\S+"', decoded_line)
+#    print(url)
+    if url:
+#        newurl = url[0]
+        url_lst.extend(url)
 
-# print(newurl,url_lst)
+print(len(url_lst))
