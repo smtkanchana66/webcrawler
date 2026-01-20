891a6084d372f3eb33401357d65a9831a2075f44
smtkanchana66 <ict20066@std.uwu.ac.lk>
2026-01-20 08:48:03 +0530

    Add loop to iterate finded links and crawl them but not working fuckedup

--- webcrawler.py
+++ webcrawler.py
@@ -1,15 +1,30 @@
 import urllib.request, urllib.parse, urllib.error
 import re 
 url_lst = list()
+temp_url = 'https://pahe.ink/'
 
-fhand = urllib.request.urlopen('https://pahe.ink/')
+while True:
+    fhand = urllib.request.urlopen(temp_url)
+    print("This is temp url",temp_url,len(url_lst))
 
-for line in fhand:
-    decoded_line = line.decode('utf-8', errors='ignore').strip()
-    url = re.findall('https?://\\S+"', decoded_line)
-#    print(url)
-    if url:
-#        newurl = url[0]
-        url_lst.extend(url)
+    for line in fhand:
+        decoded_line = line.decode('utf-8', errors='ignore').strip()
+        url = re.findall('https?://\\S+"', decoded_line)
+        print(url)
+
+        if url:
+            if url[0] not in url_lst:            
+                url_lst.extend(url)
+            print(url_lst)
+            
+        if url_lst:
+            with open("Url_list.txt", "w") as url_file:
+                url_file.write(url_lst[0])
+            temp_url = url_lst[0]
+            print(url_lst[0])
+            url_lst.remove(url_lst[0])
+            print(url_lst[0])
+        else:
+            break
 
 print(len(url_lst))
